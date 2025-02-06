c30967f95204f82e6a4a2861a8fabdff35a3a06c
Jasper <jasperxu1117@gmail.com>
2025-02-06 09:14:08 +0800

    Initial Commit

---
+++ SwiftCompletionAndAsyncNetworking/SwiftCompletionAndAsyncNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCompletionAndAsyncNetworkingApp.swift
+//  SwiftCompletionAndAsyncNetworking
+//
+//  Created by Jasper Hui on 6/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCompletionAndAsyncNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftCompletionAndAsyncNetworkingTests/SwiftCompletionAndAsyncNetworkingTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCompletionAndAsyncNetworkingTests.swift
+//  SwiftCompletionAndAsyncNetworkingTests
+//
+//  Created by Jasper Hui on 6/2/2025.
+//
+
+import Testing
+@testable import SwiftCompletionAndAsyncNetworking
+
+struct SwiftCompletionAndAsyncNetworkingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
